4a2cae3ed1640172771b835ad6146f934347d3e6
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-10-01 13:58:28 -0300

    Add utility and parsing routines

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAXTOKENLEN 64
 #define MAXTOKENS   128
@@ -16,6 +18,11 @@ enum token_type {
 
 /* main token stack */
 struct token stack[MAXTOKENS];
+int stack_index = -1;
+
+#define push(A)	stack[++stack_index] = (A)
+#define pop	stack[stack_index--]
+#define current	stack[stack_index]
 
 /* current token */
 struct token this;
@@ -49,6 +56,111 @@ classify_string()
     }
 
     return IDENTIFIER;
+}
+
+/* reads the next token into `this` and classify it */
+void
+get_token()
+{
+    char *str = this.string;
+
+    /* discard whitespace */
+    while (isspace(*str = getchar())) ;
+
+    if (isalnum(*str)) {
+	/* read the rest */
+	while (isalnum(*++str = getchar())) ;
+	ungetc(*str, stdin);
+	*str = '\0';
+
+	this.type = classify_string();
+	return;
+    }
+
+    /* for other not alnum the token type is itself */
+    this.type = *str;
+    *++str = '\0';
+
+    return;
+}
+
+void
+read_to_first_identifier()
+{
+    do {
+	get_token();
+	push(this);
+    } while (this.type != IDENTIFIER);
+
+    printf("%s is a ", this.string);
+    pop;
+
+    get_token();
+}
+
+/* --- Parsing Routines ---
+ *
+ * Work on the current token and always
+ * read the next one at the end.
+ */
+
+void
+deal_with_function_args()
+{
+    /* read past closing `)` */
+    while (this.type != ')') get_token();
+    get_token();
+
+    printf("function returning ");
+}
+
+void
+deal_with_arrays()
+{
+    printf("array ");
+    get_token(); /* read size or `]` */
+
+    /* print size if given */
+    if (isdigit(*this.string)) {
+	printf("size %s ", this.string);
+	get_token(); /* read `]` */
+    }
+
+    printf("of ");
+    get_token();
+}
+
+void
+deal_with_pointers()
+{
+    while (current.type == '*') {
+	printf("pointer to ");
+	pop;
+    }
+}
+
+void
+deal_with_declarator()
+{
+    if (this.type == '[')
+	deal_with_arrays();
+
+    if (this.type == '(')
+	deal_with_function_args();
+
+    deal_with_pointers();
+
+    /* while there is content on the stack */
+    while (stack_index >= 0) {
+	if (current.type == '(') {
+	    /* simple parenthesis */
+	    pop;
+	    get_token(); /* should be the matching `)` */
+	    deal_with_declarator();
+	} else {
+	    printf("%s ", pop.string);
+	}
+    }
 }
 
 int
