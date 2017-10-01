1361cfa401b41fd6e5dea2ba49a52f27693de644
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-10-01 11:35:29 -0300

    Add type declarations and first function

--- main.c
+++ main.c
@@ -1,3 +1,56 @@
+#include <string.h>
+
+#define MAXTOKENLEN 64
+#define MAXTOKENS   128
+
+struct token {
+    char type;
+    char string[MAXTOKENLEN];
+};
+
+enum token_type {
+    TYPE,
+    QUALIFIER,
+    IDENTIFIER
+};
+
+/* main token stack */
+struct token stack[MAXTOKENS];
+
+/* current token */
+struct token this;
+
+/* returns the type of the current token */
+enum token_type
+classify_string()
+{
+    char *str = this.string;
+
+    if  (  !strcmp(str, "void")
+	|| !strcmp(str, "char")
+	|| !strcmp(str, "short")
+	|| !strcmp(str, "int")
+	|| !strcmp(str, "long")
+	|| !strcmp(str, "signed")
+	|| !strcmp(str, "unsigned")
+	|| !strcmp(str, "float")
+	|| !strcmp(str, "double")
+	|| !strcmp(str, "struct")
+	|| !strcmp(str, "union")
+	|| !strcmp(str, "enum")
+    ) return TYPE;
+
+    if  (!strcmp(str, "volatile"))
+	return QUALIFIER;
+
+    if  (!strcmp(str, "const")) {
+	strcpy(str, "readonly");
+	return QUALIFIER;
+    }
+
+    return IDENTIFIER;
+}
+
 int
 main(int argc, char **argv)
 {
